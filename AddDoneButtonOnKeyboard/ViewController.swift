ace7ce56cd04e567bccb6134a416e776bdd713bd
Jeongmin <tdcian71@gmail.com>
2020-12-29 12:01:10 +0900

    키보드에 '완료' 버튼을 붙이기 위해 addDoneButtonOnKeyboard() 메소드 추가

--- AddDoneButtonOnKeyboard/ViewController.swift
+++ AddDoneButtonOnKeyboard/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.addDoneButtonOnKeyboard()
     }
 
 
 }
 
+extension UITextField {
+    
+    func addDoneButtonOnKeyboard() {
+        
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(self.doneButtonAction))
+        done.tintColor = .black
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+        
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+    
+}
