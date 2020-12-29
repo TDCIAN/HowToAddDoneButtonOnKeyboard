b91b0e24df70d9b8b51ff0ba5238fbe24cefe25c
Jeongmin <tdcian71@gmail.com>
2020-12-29 12:13:28 +0900

    텍스트뷰 추가

--- AddDoneButtonOnKeyboard/ViewController.swift
+++ AddDoneButtonOnKeyboard/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -45,4 +47,26 @@ extension UITextField {
         self.resignFirstResponder()
     }
     
+}
+
+extension UITextView {
+    func addDoneButtonOnKeyboard() {
+        
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(self.doneButtonAction))
+        done.tintColor = .black
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+        
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
 }
